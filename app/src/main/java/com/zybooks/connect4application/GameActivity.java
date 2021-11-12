af903801b34225885c42d4423a75b96a138f3aa6
JMisley <johnny.misley@gmail.com>
2021-11-12 12:09:13 -0500

    yes

--- app/src/main/java/com/zybooks/connect4application/GameActivity.java
+++ app/src/main/java/com/zybooks/connect4application/GameActivity.java
@@ -55,6 +55,13 @@ public class GameActivity extends AppCompatActivity {
         viewHolder.turnIndicatorImageView.setImageResource(resourceForTurn());
         viewHolder.winnerText = (TextView) findViewById(R.id.winner_text);
         viewHolder.winnerText.setVisibility(View.GONE);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.black));
+        }
     }
 
 
--- app/src/main/java/com/zybooks/connect4application/StartActivity.java
+++ app/src/main/java/com/zybooks/connect4application/StartActivity.java
@@ -21,7 +21,12 @@ public class StartActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.black));
+        }
 
         Intent intent = new Intent(this, BackgroundSoundService.class);
         startService(intent);
