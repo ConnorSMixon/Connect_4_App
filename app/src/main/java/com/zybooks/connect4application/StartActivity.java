70d05204f7cfd9c652b29fb6fa578091f68a81af
ConnorSMixon <connormixon@yahoo.com>
2021-11-12 11:41:30 -0500

    Commiting Game Activity

--- app/src/main/java/com/zybooks/connect4application/StartActivity.java
+++ app/src/main/java/com/zybooks/connect4application/StartActivity.java
@@ -21,44 +21,11 @@ public class StartActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        if (Build.VERSION.SDK_INT >= 21) {
-            Window window = this.getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.setStatusBarColor(this.getResources().getColor(R.color.black));
-        }
+
 
         Intent intent = new Intent(this, BackgroundSoundService.class);
         startService(intent);
 
-    }
-
-    public void createPieceAnimation(int pieceID) {
-
-        Random randPiece = new Random();
-        int piece = randPiece.nextInt(2)+1;
-
-        switch (piece) {
-            case 1:
-                pieceID = R.drawable.red_piece;
-                break;
-            case 2:
-                pieceID = R.drawable.yellow_piece;
-                break;
-        }
-
-        ImageView imageView = (ImageView) findViewById(pieceID);
-
-        imageView.setVisibility(imageView.VISIBLE);
-
-        float bottomOfScreen = getResources().getDisplayMetrics()
-                .heightPixels - (imageView.getHeight() * 4);
-        //bottomOfScreen is where you want to animate to
-
-        imageView.animate()
-                .translationY(bottomOfScreen)
-                .setInterpolator(new AccelerateInterpolator())
-                .setDuration(10000);
     }
 
     // Activity Methods
