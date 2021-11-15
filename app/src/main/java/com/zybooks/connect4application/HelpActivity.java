05331192c8649904110ab4723edd7fcda2626ad7
MendokusaiCode <59923721+MendokusaiCode@users.noreply.github.com>
2021-11-15 13:43:39 -0500

    Help Activity xml and front end done and linked with buttons for more info or not.

--- app/src/main/java/com/zybooks/connect4application/HelpActivity.java
+++ app/src/main/java/com/zybooks/connect4application/HelpActivity.java
@@ -1,14 +1,19 @@
 package com.zybooks.connect4application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class HelpActivity extends AppCompatActivity {
 
+
+    private Object NeedInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,15 @@ public class HelpActivity extends AppCompatActivity {
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.setStatusBarColor(this.getResources().getColor(R.color.black));
         }
+
+    }
+    public void onNeedHelpClick(View view) {
+        Intent info = new Intent(this, NeedInfo.class);
+        startActivity(info);
+    }
+    public void onNeedNoHelpClick(View view) {
+        Intent noInfo = new Intent(this, DontNeedInfo.class);
+        startActivity(noInfo);
     }
 
 }
