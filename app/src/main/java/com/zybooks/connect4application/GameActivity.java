9e0fcf9aeed1469f70402e849037f525859d12ce
JMisley <johnny.misley@gmail.com>
2021-11-17 12:03:37 -0500

    Colors Logic

--- app/src/main/java/com/zybooks/connect4application/GameActivity.java
+++ app/src/main/java/com/zybooks/connect4application/GameActivity.java
@@ -2,13 +2,10 @@ package com.zybooks.connect4application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
